37c40d65c23f613496be1e6c28601f34cc50faae
Antonio Calanducci <info@etnatraining.it>
2013-07-08 18:56:23 +0200

    build script

---
+++ run.sh
@@ -0,0 +1,3 @@
+alloy compile --config platform=ios
+"/Users/etna/Library/Application Support/Titanium/mobilesdk/osx/3.1.0.GA/iphone/ios-sim" launch "/Users/etna/labs/gLibrary-mobile/build/iphone/build/Debug-iphonesimulator/gLibrary Upload.app" --sdk 6.0 --family iphone &
+tail -f "/Users/etna/Library/Application Support/iPhone Simulator/6.0/Applications/81D12321-7503-4EE6-88B7-24179827AA08/Documents/3839317d-dfdb-4ec9-8710-7fc75c8124c2.log"
